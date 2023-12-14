1b96589a8a0a5c48477647889870039349a5dfa1
Bari <84495600+cerebralfracture@users.noreply.github.com>
2023-12-14 17:32:34 +0700

    Update trainSpeakerNet.py

--- trainSpeakerNet.py
+++ trainSpeakerNet.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*-
+#-*- coding: utf-8 -*-
 
-import sys
-import time
-import os
-import argparse
+import sys, time, os, argparse
 import yaml
 import numpy
 import torch
@@ -24,72 +21,72 @@ warnings.simplefilter("ignore")
 ## Parse arguments
 ## ===== ===== ===== ===== ===== ===== ===== =====
 
-parser = argparse.ArgumentParser(description="SpeakerNet")
+parser = argparse.ArgumentParser(description = "SpeakerNet")
 
-parser.add_argument('--config', type=str, default=None, help='Config YAML file')
+parser.add_argument('--config',         type=str,   default=None,   help='Config YAML file')
 
 ## Data loader
-parser.add_argument('--max_frames', type=int, default=200, help='Input length to the network for training')
-parser.add_argument('--eval_frames', type=int, default=300, help='Input length to the network for testing 0 uses the whole files')
-parser.add_argument('--batch_size', type=int, default=200, help='Batch size, number of speakers per batch')
-parser.add_argument('--max_seg_per_spk', type=int, default=500, help='Maximum number of utterances per speaker per epoch')
-parser.add_argument('--nDataLoaderThread', type=int, default=5, help='Number of loader threads')
-parser.add_argument('--augment', type=bool, default=False, help='Augment input')
-parser.add_argument('--seed', type=int, default=10, help='Seed for the random number generator')
+parser.add_argument('--max_frames',     type=int,   default=200,    help='Input length to the network for training')
+parser.add_argument('--eval_frames',    type=int,   default=300,    help='Input length to the network for testing 0 uses the whole files')
+parser.add_argument('--batch_size',     type=int,   default=200,    help='Batch size, number of speakers per batch')
+parser.add_argument('--max_seg_per_spk', type=int,  default=500,    help='Maximum number of utterances per speaker per epoch')
+parser.add_argument('--nDataLoaderThread', type=int, default=5,     help='Number of loader threads')
+parser.add_argument('--augment',        type=bool,  default=False,  help='Augment input')
+parser.add_argument('--seed',           type=int,   default=10,     help='Seed for the random number generator')
 
 ## Training details
-parser.add_argument('--test_interval', type=int, default=10, help='Test and save every [test_interval] epochs')
-parser.add_argument('--max_epoch', type=int, default=100, help='Maximum number of epochs')
-parser.add_argument('--trainfunc', type=str, default="", help='Loss function')
+parser.add_argument('--test_interval',  type=int,   default=10,     help='Test and save every [test_interval] epochs')
+parser.add_argument('--max_epoch',      type=int,   default=100,    help='Maximum number of epochs')
+parser.add_argument('--trainfunc',      type=str,   default="",     help='Loss function')
 
 ## Optimizer
-parser.add_argument('--optimizer', type=str, default="adam", help='sgd or adam')
-parser.add_argument('--scheduler', type=str, default="steplr", help='Learning rate scheduler')
-parser.add_argument('--lr', type=float, default=0.001, help='Learning rate')
-parser.add_argument("--lr_decay", type=float, default=0.95, help='Learning rate decay every [test_interval] epochs')
-parser.add_argument('--weight_decay', type=float, default=0, help='Weight decay in the optimizer')
+parser.add_argument('--optimizer',      type=str,   default="adam", help='sgd or adam')
+parser.add_argument('--scheduler',      type=str,   default="steplr", help='Learning rate scheduler')
+parser.add_argument('--lr',             type=float, default=0.001,  help='Learning rate')
+parser.add_argument("--lr_decay",       type=float, default=0.95,   help='Learning rate decay every [test_interval] epochs')
+parser.add_argument('--weight_decay',   type=float, default=0,      help='Weight decay in the optimizer')
 
 ## Loss functions
-parser.add_argument("--hard_prob", type=float, default=0.5, help='Hard negative mining probability, otherwise random, only for some loss functions')
-parser.add_argument("--hard_rank", type=int, default=10, help='Hard negative mining rank in the batch, only for some loss functions')
-parser.add_argument('--margin', type=float, default=0.1, help='Loss margin, only for some loss functions')
-parser.add_argument('--scale', type=float, default=30, help='Loss scale, only for some loss functions')
-parser.add_argument('--nPerSpeaker', type=int, default=1, help='Number of utterances per speaker per batch, only for metric learning based losses')
-parser.add_argument('--nClasses', type=int, default=5994, help='Number of speakers in the softmax layer, only for softmax-based losses')
+parser.add_argument("--hard_prob",      type=float, default=0.5,    help='Hard negative mining probability, otherwise random, only for some loss functions')
+parser.add_argument("--hard_rank",      type=int,   default=10,     help='Hard negative mining rank in the batch, only for some loss functions')
+parser.add_argument('--margin',         type=float, default=0.1,    help='Loss margin, only for some loss functions')
+parser.add_argument('--scale',          type=float, default=30,     help='Loss scale, only for some loss functions')
+parser.add_argument('--nPerSpeaker',    type=int,   default=1,      help='Number of utterances per speaker per batch, only for metric learning based losses')
+parser.add_argument('--nClasses',       type=int,   default=5994,   help='Number of speakers in the softmax layer, only for softmax-based losses')
 
 ## Evaluation parameters
-parser.add_argument('--dcf_p_target', type=float, default=0.05, help='A priori probability of the specified target speaker')
-parser.add_argument('--dcf_c_miss', type=float, default=1, help='Cost of a missed detection')
-parser.add_argument('--dcf_c_fa', type=float, default=1, help='Cost of a spurious detection')
+parser.add_argument('--dcf_p_target',   type=float, default=0.05,   help='A priori probability of the specified target speaker')
+parser.add_argument('--dcf_c_miss',     type=float, default=1,      help='Cost of a missed detection')
+parser.add_argument('--dcf_c_fa',       type=float, default=1,      help='Cost of a spurious detection')
 
 ## Load and save
-parser.add_argument('--initial_model', type=str, default="", help='Initial model weights')
-parser.add_argument('--save_path', type=str, default="exps/exp1", help='Path for model and logs')
+parser.add_argument('--initial_model',  type=str,   default="",     help='Initial model weights')
+parser.add_argument('--save_path',      type=str,   default="exps/exp1", help='Path for model and logs')
 
 ## Training and test data
-parser.add_argument('--train_list', type=str, default="data/train_list.txt", help='Train list')
-parser.add_argument('--test_list', type=str, default="data/test_list.txt", help='Evaluation list')
-parser.add_argument('--train_path', type=str, default="data/voxceleb2", help='Absolute path to the train set')
-parser.add_argument('--test_path', type=str, default="data/voxceleb1", help='Absolute path to the test set')
-parser.add_argument('--musan_path', type=str, default="data/musan_split", help='Absolute path to the test set')
-parser.add_argument('--rir_path', type=str, default="data/RIRS_NOISES/simulated_rirs", help='Absolute path to the test set')
+parser.add_argument('--train_list',     type=str,   default="data/train_list.txt",  help='Train list')
+parser.add_argument('--test_list',      type=str,   default="data/test_list.txt",   help='Evaluation list')
+parser.add_argument('--train_path',     type=str,   default="data/voxceleb2", help='Absolute path to the train set')
+parser.add_argument('--test_path',      type=str,   default="data/voxceleb1", help='Absolute path to the test set')
+parser.add_argument('--musan_path',     type=str,   default="data/musan_split", help='Absolute path to the test set')
+parser.add_argument('--rir_path',       type=str,   default="data/RIRS_NOISES/simulated_rirs", help='Absolute path to the test set')
 
 ## Model definition
-parser.add_argument('--n_mels', type=int, default=40, help='Number of mel filterbanks')
-parser.add_argument('--log_input', type=bool, default=False, help='Log input features')
-parser.add_argument('--model', type=str, default="", help='Name of model definition')
-parser.add_argument('--encoder_type', type=str, default="SAP", help='Type of encoder')
-parser.add_argument('--nOut', type=int, default=512, help='Embedding size in the last FC layer')
-parser.add_argument('--sinc_stride', type=int, default=10, help='Stride size of the first analytic filterbank layer of RawNet3')
+parser.add_argument('--n_mels',         type=int,   default=40,     help='Number of mel filterbanks')
+parser.add_argument('--log_input',      type=bool,  default=False,  help='Log input features')
+parser.add_argument('--model',          type=str,   default="",     help='Name of model definition')
+parser.add_argument('--encoder_type',   type=str,   default="SAP",  help='Type of encoder')
+parser.add_argument('--nOut',           type=int,   default=512,    help='Embedding size in the last FC layer')
+parser.add_argument('--sinc_stride',    type=int,   default=10,    help='Stride size of the first analytic filterbank layer of RawNet3')
 
 ## For test only
-parser.add_argument('--eval', dest='eval', action='store_true', help='Eval only')
-parser.add_argument('--eval_out', dest='eval_out', action='store_true', help='Eval only')
+parser.add_argument('--eval',           dest='eval', action='store_true', help='Eval only')
+parser.add_argument('--eval_out',           dest='eval_out', action='store_true', help='Eval only')
 
 ## Distributed and mixed precision training
-parser.add_argument('--port', type=str, default="8888", help='Port for distributed training, input as text')
-parser.add_argument('--distributed', dest='distributed', action='store_true', help='Enable distributed training')
-parser.add_argument('--mixedprec', dest='mixedprec', action='store_true', help='Enable mixed precision training')
+parser.add_argument('--port',           type=str,   default="8888", help='Port for distributed training, input as text')
+parser.add_argument('--distributed',    dest='distributed', action='store_true', help='Enable distributed training')
+parser.add_argument('--mixedprec',      dest='mixedprec',   action='store_true', help='Enable mixed precision training')
 
 args = parser.parse_args()
 
@@ -97,7 +94,7 @@ args = parser.parse_args()
 def find_option_type(key, parser):
     for opt in parser._get_optional_actions():
         if ('--' + key) in opt.option_strings:
-            return opt.type
+           return opt.type
     raise ValueError
 
 if args.config is not None:
@@ -109,6 +106,7 @@ if args.config is not None:
             args.__dict__[k] = typ(v)
         else:
             sys.stderr.write("Ignored unknown parameter {} in yaml.\n".format(k))
+
 
 ## ===== ===== ===== ===== ===== ===== ===== =====
 ## Trainer script
@@ -122,8 +120,8 @@ def main_worker(gpu, ngpus_per_node, args):
     s = SpeakerNet(**vars(args))
 
     if args.distributed:
-        os.environ['MASTER_ADDR'] = 'localhost'
-        os.environ['MASTER_PORT'] = args.port
+        os.environ['MASTER_ADDR']='localhost'
+        os.environ['MASTER_PORT']=args.port
 
         dist.init_process_group(backend='nccl', world_size=ngpus_per_node, rank=args.gpu)
 
@@ -142,8 +140,8 @@ def main_worker(gpu, ngpus_per_node, args):
 
     if args.gpu == 0:
         ## Write args to scorefile
-        scorefile = open(args.result_save_path + "/scores.txt", "a+")
-        scorefile2 = open(args.result_save_path + "/scores_eer.txt", "a+")
+        scorefile   = open(args.result_save_path+"/scores.txt", "a+")
+        scorefile2 = open(args.result_save_path+"/scores_eer.txt", "a+")
 
     ## Initialise trainer and data loader
     train_dataset = train_dataset_loader(**vars(args))
@@ -160,67 +158,65 @@ def main_worker(gpu, ngpus_per_node, args):
         drop_last=True,
     )
 
-    trainer = ModelTrainer(s, **vars(args))
+    trainer     = ModelTrainer(s, **vars(args))
 
     ## Load model weights
-    modelfiles = glob.glob('%s/model0*.model' % args.model_save_path)
+    modelfiles = glob.glob('%s/model0*.model'%args.model_save_path)
     modelfiles.sort()
 
-    if args.initial_model != "":
+    if(args.initial_model != ""):
         trainer.loadParameters(args.initial_model)
-        print("Модель {} загружена!".format(args.initial_model))
+        print("Model {} loaded!".format(args.initial_model))
     elif len(modelfiles) >= 1:
         trainer.loadParameters(modelfiles[-1])
         print("Model {} loaded from previous state!".format(modelfiles[-1]))
         it = int(os.path.splitext(os.path.basename(modelfiles[-1]))[0][5:]) + 1
 
-    for it in range(it, args.max_epoch + 1):
+    for ii in range(1,it):
         trainer.__scheduler__.step()
-        train_sampler.set_epoch(it)
-        clr = [x['lr'] for x in trainer.__optimizer__.param_groups]
-        loss, traineer = trainer.train_network(train_loader, verbose=(args.gpu == 0))
 
     ## Evaluation code - must run on single GPU
-    if it % args.test_interval == 0:
+    if args.eval == True:
 
         pytorch_total_params = sum(p.numel() for p in s.module.__S__.parameters())
 
-        print('Total parameters: ', pytorch_total_params)
-        print('Test list', args.test_list)
-
+        print('Total parameters: ',pytorch_total_params)
+        print('Test list',args.test_list)
+        
         sc, lab, _ = trainer.evaluateFromList(**vars(args))
 
         if args.gpu == 0:
 
-            print('\n', time.strftime("%Y-%m-%d %H:%M:%S"),
-                  "Epoch {:d}, Acc {:2.2f}, LOSS {:f}, LR {:f}".format(it, traineer, loss, max(clr)))
-        scorefile.write("{:d} {:2.2f} {:f} {:f}\n".format(it, traineer, loss, max(clr)))
+            result = tuneThresholdfromScore(sc, lab, [1, 0.1])
 
-        trainer.saveParameters(args.model_save_path + "/model%09d.model" % it)
-        with open(args.model_save_path + "/model%09d.eer" % it, 'w') as eerfile:
-            eerfile.write('{:2.4f}'.format(result[1]))
+            fnrs, fprs, thresholds = ComputeErrorRates(sc, lab)
+            mindcf, threshold = ComputeMinDcf(fnrs, fprs, thresholds, args.dcf_p_target, args.dcf_c_miss, args.dcf_c_fa)
+
+            print('\n',time.strftime("%Y-%m-%d %H:%M:%S"), "EER {:2.4f}".format(result[1]), "MinDCF {:2.5f}".format(mindcf))
 
         return
-
+        
     ## Evaluation code - must run on single GPU
     if args.eval_out == True:
 
         pytorch_total_params = sum(p.numel() for p in s.module.__S__.parameters())
 
-        print('Total parameters: ', pytorch_total_params)
-        print('Test list', args.test_list)
-
+        print('Total parameters: ',pytorch_total_params)
+        print('Test list',args.test_list)
+        
         sc, lab, _ = trainer.evaluateFromList(**vars(args))
-        # sc.to("cpu")
-        # lab.to("cpu")
+        #sc.to("cpu")
+        #lab.to("cpu")
         print(len(sc))
+        
 
-        # if args.gpu == 0:
+        #if args.gpu == 0:
 
         #    result = tuneThresholdfromScore(sc, lab, [1, 0.1])
-
+            
         with open('test.pickle', 'wb') as handle:
             pickle.dump(sc, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    
 
         return
 
@@ -229,52 +225,47 @@ def main_worker(gpu, ngpus_per_node, args):
         pyfiles = glob.glob('./*.py')
         strtime = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
 
-        zipf = zipfile.ZipFile(args.result_save_path + '/run%s.zip' % strtime, 'w', zipfile.ZIP_DEFLATED)
+        zipf = zipfile.ZipFile(args.result_save_path+ '/run%s.zip'%strtime, 'w', zipfile.ZIP_DEFLATED)
         for file in pyfiles:
             zipf.write(file)
         zipf.close()
 
-        with open(args.result_save_path + '/run%s.cmd' % strtime, 'w') as f:
-            f.write('%s' % args)
+        with open(args.result_save_path + '/run%s.cmd'%strtime, 'w') as f:
+            f.write('%s'%args)
 
     ## Core training script
-    for it in range(it, args.max_epoch + 1):
+for it in range(it, args.max_epoch + 1):
+    train_sampler.set_epoch(it)
+    clr = [x['lr'] for x in trainer.__optimizer__.param_groups]
+    loss, traineer = trainer.train_network(train_loader, verbose=(args.gpu == 0))
 
-        train_sampler.set_epoch(it)
+    if args.gpu == 0:
+        print('\n', time.strftime("%Y-%m-%d %H:%M:%S"), "Epoch {:d}, Acc {:2.2f}, LOSS {:f}, LR {:f}".format(it, traineer, loss, max(clr)))
+        scorefile.write("{:d} {:2.2f} {:f} {:f}\n".format(it, traineer, loss, max(clr)))
 
-        clr = [x['lr'] for x in trainer.__optimizer__.param_groups]
-
-        loss, traineer = trainer.train_network(train_loader, verbose=(args.gpu == 0))
+    if it % args.test_interval == 0:
+        sc, lab, _ = trainer.evaluateFromList(**vars(args))
 
         if args.gpu == 0:
-            print('\n',time.strftime("%Y-%m-%d %H:%M:%S"), "Epoch {:d}, Acc {:2.2f}, LOSS {:f}, LR {:f}".format(it, traineer, loss, max(clr)))
-            scorefile.write("{:d} {:2.2f} {:f} {:f}\n".format(it, traineer, loss, max(clr)))
+            result = tuneThresholdfromScore(sc, lab, [1, 0.1])
 
-        if it % args.test_interval == 0:
+            fnrs, fprs, thresholds = ComputeErrorRates(sc, lab)
+            mindcf, threshold = ComputeMinDcf(fnrs, fprs, thresholds, args.dcf_p_target, args.dcf_c_miss, args.dcf_c_fa)
 
-            sc, lab, _ = trainer.evaluateFromList(**vars(args))
+            eers.append(result[1])
 
-            if args.gpu == 0:
-                
-                result = tuneThresholdfromScore(sc, lab, [1, 0.1])
+            print('\n')
+            print('\n Validation: ', time.strftime("%Y-%m-%d %H:%M:%S"), "Epoch {:d}, EER {:2.4f}, MinDCF {:2.5f}".format(it, result[1], mindcf))
+            print('\n')
 
-                fnrs, fprs, thresholds = ComputeErrorRates(sc, lab)
-                mindcf, threshold = ComputeMinDcf(fnrs, fprs, thresholds, args.dcf_p_target, args.dcf_c_miss, args.dcf_c_fa)
+            last_result = result  # Запоминаем текущий результат
+            trainer.saveParameters(args.model_save_path + "/model%09d.model" % it)
 
-                eers.append(result[1])
-
-                print('\n')
-                print('\n Validation: ',time.strftime("%Y-%m-%d %H:%M:%S"), "Epoch {:d}, EER {:2.4f}, MinDCF {:2.5f}".format(it, result[1], mindcf))
-                print('\n')
-                #scorefile.write("Epoch {:d}, VEER {:2.4f}, MinDCF {:2.5f}\n".format(it, result[1], mindcf))
-
-                trainer.saveParameters(args.model_save_path+"/model%09d.model"%it)
-
-                with open(args.model_save_path+"/model%09d.eer"%it, 'w') as eerfile:
-                    eerfile.write('{:2.4f}'.format(result[1]))
-                scorefile2.write('{:2.4f}'.format(result[1])+'\n')
-                scorefile.flush()
-                scorefile2.flush()
+            with open(args.model_save_path + "/model%09d.eer" % it, 'w') as eerfile:
+                eerfile.write('{:2.4f}'.format(result[1]))
+            scorefile2.write('{:2.4f}'.format(result[1]) + '\n')
+            scorefile.flush()
+            scorefile2.flush()
 
     if args.gpu == 0:
         scorefile.close()
